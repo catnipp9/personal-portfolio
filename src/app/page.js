fe50eae53a1037dc48966a68fe76777545cdf64c
Strawberry Jam <153965337+catnipp9@users.noreply.github.com>
2025-05-16 13:13:34 +0800

    quick fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { NavBar } from '@/app/components/NavBar';
 import { Banner } from '@/app/components/Banner';
 import { AboutMe } from '@/app/components/AboutMe';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Home() {
